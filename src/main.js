ace9e22dd2c4089b4fe44524e521b70b40164858
Valentin GOT <gotvalentin@gmail.com>
2017-02-07 15:45:22 +0100

    refactor(main): remove spaces

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './app/app.component.vue'
 
-
 new Vue({
-    el        : '#app',
-    render    : h => h(App)
-})
+  el    : '#app',
+  render: h => h(App)
+});
