36f80a686c7b2e01b9a5b1cfa92827291ef0e8da
Valentin GOT <gotvalentin@gmail.com>
2017-02-07 15:45:53 +0100

    feat(my-component): add a test component

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,23 +1,26 @@
 import VueGridLayout from 'vue-grid-layout';
 
-var testLayout = [
-    {"x": 0, "y": 0, "w": 1, "h": 1, "i": "0"},
-    {"x": 1, "y": 0, "w": 1, "h": 1, "i": "1"},
-    {"x": 2, "y": 0, "w": 1, "h": 1, "i": "2"},
-    {"x": 3, "y": 0, "w": 1, "h": 1, "i": "3"},
-    {"x": 4, "y": 0, "w": 1, "h": 1, "i": "4"}
+import TestComponent from './test/test.component.vue';
+
+let testLayout = [
+  {"x": 0, "y": 0, "w": 1, "h": 1, "i": "0"},
+  {"x": 1, "y": 0, "w": 1, "h": 1, "i": "1"},
+  {"x": 2, "y": 0, "w": 1, "h": 1, "i": "2"},
+  {"x": 3, "y": 0, "w": 1, "h": 1, "i": "3"},
+  {"x": 4, "y": 0, "w": 1, "h": 1, "i": "4"}
 
 ];
 
 export default {
-    name      : 'app',
-    components: {
-        GridLayout: VueGridLayout.GridLayout,
-        GridItem: VueGridLayout.GridItem
-    },
-    data () {
-        return {
-            layout: testLayout
-        }
+  name      : 'app',
+  components: {
+    GridLayout    : VueGridLayout.GridLayout,
+    GridItem      : VueGridLayout.GridItem,
+    'my-component': TestComponent
+  },
+  data () {
+    return {
+      layout: testLayout
     }
+  }
 }
---
+++ src/app/test/test.component.vue
@@ -0,0 +1,3 @@
+<template src="./test.component.html"></template>
+<style src="./test.component.css"></style>
+<script src="./test.component.js"></script>
