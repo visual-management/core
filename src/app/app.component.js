91451b3e74f1922ea53596058a37317d9adfbbba
Valentin GOT <gotvalentin@gmail.com>
2017-02-08 10:13:16 +0100

    feat(grid): add an editing / not editing mode

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,26 +1,34 @@
 import VueGridLayout from 'vue-grid-layout';
-
 import TestComponent from './test/test.component.vue';
 
 let testLayout = [
-  {"x": 0, "y": 0, "w": 1, "h": 1, "i": "0"},
-  {"x": 1, "y": 0, "w": 1, "h": 1, "i": "1"},
-  {"x": 2, "y": 0, "w": 1, "h": 1, "i": "2"},
-  {"x": 3, "y": 0, "w": 1, "h": 1, "i": "3"},
-  {"x": 4, "y": 0, "w": 1, "h": 1, "i": "4"}
-
+  { x: 0, y: 0, w: 1, h: 1, i: "0" },
+  { x: 1, y: 0, w: 1, h: 1, i: "1" },
+  { x: 2, y: 0, w: 1, h: 1, i: "2" },
+  { x: 3, y: 0, w: 1, h: 1, i: "3" },
+  { x: 4, y: 0, w: 1, h: 1, i: "4" }
 ];
 
 export default {
-  name      : 'app',
+  name: 'app',
+
   components: {
-    GridLayout    : VueGridLayout.GridLayout,
-    GridItem      : VueGridLayout.GridItem,
+    'grid-layout' : VueGridLayout.GridLayout,
+    'grid-item'   : VueGridLayout.GridItem,
     'my-component': TestComponent
   },
+
   data () {
     return {
-      layout: testLayout
+      editing: true,
+      layout : testLayout
+    };
+  },
+
+  methods: {
+    onEdit () {
+      this.editing = !this.editing;
     }
   }
+
 }
