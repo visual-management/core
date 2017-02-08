b76b6e6778791ae3a8dd2cf3ad1abb4a6447e384
Valentin GOT <gotvalentin@gmail.com>
2017-02-08 15:43:32 +0100

    feat(server): add mongoose and mongodb connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,20 @@
-const app = require('http').createServer(),
-  io = require('socket.io')(app);
+const app   = require('http').createServer(),
+  io        = require('socket.io')(app),
+  mongoose  = require('mongoose'),
+  config    = require('config');
 
 app.listen(9090);
 
+// Socket.IO
 io.on('connection', (socket) => {
   console.log('CONNECTED');
 });
+
+// MongoDB
+let uri = `${config.get('MONGODB.HOST')}:${config.get('MONGODB.PORT')}/${config.get('MONGODB.DATABASE')}`;
+if (config.has('MONGODB.USERNAME') && config.has('MONGODB.PASSWORD')) {
+  uri = `${config.get('MONGODB.USERNAME')}:${config.get('MONGODB.PASSWORD')}@` + uri;
+}
+
+mongoose.connect(uri);
+
