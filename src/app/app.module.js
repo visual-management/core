cf437043174795233036365f3951b21e307ff2e6
Olivier LE LAY <liollury@gmail.com>
2017-02-08 10:59:16 +0100

    chore(module): add module discovery from npm link of VM-pm

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,12 +1,17 @@
 import VueGridLayout from 'vue-grid-layout';
 
-import SonarPlugin from './sonar-plugin/entry-point';
+import manifest from 'visual-management-pm';
+import SonarPlugin from 'visual-management-pm/src/app/sonar-plugin/entry-point';
 let plugins = [SonarPlugin];
 
 let components = {
   GridLayout: VueGridLayout.GridLayout,
   GridItem  : VueGridLayout.GridItem
 };
+
+for (let plugin of manifest.plugins) {
+  console.log('Plugin ' + plugin.name + ' is available.');
+}
 
 for (let plugin of plugins) {
   console.log(`Loading plugin ${plugin.name}`);
