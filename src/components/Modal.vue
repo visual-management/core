60bdcd87325f24f7eb72003eca4dc511ecdbf20c
Kevin Grandemange <kevin@kevingrandemange.net>
2017-04-19 17:09:43 +0200

    refacto(Modal.vue): css

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="modal-content">
-    <div class="modal-header">
-      <h2>Plugins Manager</h2>
-      <span class="close" @click="close()">&times;</span>
-    </div>
-    <div class="modal-body">
-      <div v-for="plugin in plugins" v-show="showPlugin">
-        <div class="plugin-name">
-          {{ plugin.name }}
-        </div>
-        <div class="content-component">
-          <div v-for="subPlugin in plugin.components" class="component" @click="showEditor(subPlugin.config)">
-            {{ subPlugin.name }}
+  <div class="modal-container">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2>Plugins Manager</h2>
+        <span class="close" @click="close()">&times;</span>
+      </div>
+      <div class="modal-body">
+        <div v-for="plugin in plugins" v-show="showPlugin">
+          <div class="plugin-name">
+            {{ plugin.name }}
+          </div>
+          <div class="content-component">
+            <div v-for="subPlugin in plugin.components" class="component" @click="showEditor(subPlugin.config)">
+              {{ subPlugin.name }}
+            </div>
           </div>
         </div>
+        <textarea ref="editor" v-show="editor">
+        </textarea>
       </div>
-      <textarea ref="editor" v-show="editor">
-      </textarea>
-    </div>
-    <div class="modal-footer">
+      <div class="modal-footer">
+      </div>
     </div>
   </div>
 </template>
@@ -97,67 +99,42 @@
     -webkit-animation-duration: 0.4s;
     animation-name: animatetop;
     animation-duration: 0.4s;
-  }
 
-  .modal-header {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    padding: 2px 16px;
-    background-color: $primary-color;
-    color: white;
+    .modal-header {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      padding: 2px 16px;
+      background-color: $primary-color;
+      color: white;
 
-    span {
-      width: 20px;
-      cursor: pointer;
+      span {
+        width: 20px;
+        cursor: pointer;
+      }
     }
-  }
 
-  .modal-body {
-    padding: 2px 16px;
-    background-color: white;
-    display: block;
-    max-height: 55vh;
-    overflow: auto;
+    .modal-body {
+      padding: 2px 16px;
+      background-color: white;
+      display: block;
+      max-height: 55vh;
+      overflow: auto;
 
-    .content-component {
-      margin-left: 30px;
-      cursor: pointer;
+      .content-component {
+        margin-left: 30px;
+        cursor: pointer;
+      }
     }
-  }
 
-  .modal-footer {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    background-color: #5cb85c;
-    color: white;
-    padding: 10px 0;
-  }
-
-  .modal-content {
-    position: relative;
-    background-color: #fefefe;
-    margin: auto;
-    padding: 0;
-    width: 80%;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
-    -webkit-animation-name: animatetop;
-    -webkit-animation-duration: 0.4s;
-    animation-name: animatetop;
-    animation-duration: 0.4s;
-
-    &:before {
-      position: fixed;
-      top: 0;
-      left: 0;
-      content: '';
-      width: 100%;
-      height: 100%;
-      background-color: lightgray;
-      z-index: -1;
-      opacity: 0.8;
+    .modal-footer {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      background-color: #5cb85c;
+      color: white;
+      padding: 10px 0;
     }
 
     .hidden {
